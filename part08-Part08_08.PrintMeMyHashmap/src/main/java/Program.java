b868849e17d9437510f6ac79d379435bbbcc31c7
Danila <primachila@yandex.ru>
2021-07-03 17:16:14 +0200

    task 08_08
    Exercise template contains a class Program. Implement the following class methods in the class:
    
    public static void printKeys(HashMap<String,String> hashmap), prints all the keys in the hashmap given as a parameter.
    public static void printKeysWhere(HashMap<String,String> hashmap, String text) prints the keys in the hashmap given as a parameter, which contain the string given as a parameter.
    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text), prints the values in the given hashmap whichs keys contain the given string.

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -4,7 +4,40 @@ import java.util.HashMap;
 public class Program {
 
     public static void main(String[] args) {
-        // Test your program here!
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+        System.out.println("---");
+        printKeysWhere(hashmap, "i");
+        System.out.println("---");
+        printValuesOfKeysWhere(hashmap, ".e");
+    }
+    
+    public static void printKeys(HashMap<String,String> hashmap) {
+        for (String key : hashmap.keySet()) {
+            System.out.println(key);
+        }
+    }
+    
+    public static void printKeysWhere(HashMap<String,String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (!key.contains(text)) {
+                continue;
+            }
+            System.out.println(key);
+        }
+    }
+    
+    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text) {
+        for (String value : hashmap.keySet()) {
+            if (!value.contains(text)) {
+                continue;
+            }
+            System.out.println(hashmap.get(value));
+        }
     }
 
 }
