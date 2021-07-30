12d8ab607f93272ea0b8765c827a2e4394b4a574
Danila <danpry@st.amu.edu.pl>
2021-07-30 17:10:52 +0200

    10_06 Limited Numbers

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,17 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> digits = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input < 0) {
+                break;
+            }
+            digits.add(input);
+        }
+        digits.stream()
+                .filter(predicate -> predicate >= 1 &&
+                        predicate <= 5)
+                .forEach(s -> System.out.println(s));
     }
 }
