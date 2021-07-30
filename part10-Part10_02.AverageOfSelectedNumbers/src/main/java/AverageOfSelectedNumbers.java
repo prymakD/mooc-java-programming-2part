754e36628757e714e853929ad7f56a84cf773eb8
Danila <danpry@st.amu.edu.pl>
2021-07-30 15:03:02 +0200

    10_02 Average Of Selected Numbers

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,35 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        ArrayList<String> num = new ArrayList<>();
+
+        loop:
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+                String whatToPrint = scanner.nextLine();
+                if (whatToPrint.equals("p")) {
+                    double averageOfPositive = num.stream()
+                            .mapToInt(s -> Integer.valueOf(s))
+                            .filter(number -> number > 0)
+                            .average()
+                            .getAsDouble();
+                    System.out.println("Average of the positive numbers: " + averageOfPositive);
+                    break loop;
+                } else if (whatToPrint.equals("n")) {
+                    double averageOfNegative = num.stream()
+                            .mapToInt(s -> Integer.valueOf(s))
+                            .filter(number -> number < 0)
+                            .average()
+                            .getAsDouble();
+                    System.out.println("Average of the negative numbers: " + averageOfNegative);
+                    break loop;
+                }
+            }
+            num.add(input);
+        }
 
     }
 }
