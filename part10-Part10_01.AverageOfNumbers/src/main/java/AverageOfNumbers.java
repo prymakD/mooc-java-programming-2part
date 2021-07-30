7888f10dce7c84d7fefb25758fd3ede10351e5a6
Danila <danpry@st.amu.edu.pl>
2021-07-30 15:02:40 +0200

    10_01 Average of Numbers

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,21 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Write your program here
-
+        ArrayList<String> numbers = new ArrayList<>();
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("end")) {
+                double average = numbers.stream()
+                        .mapToInt(s -> Integer.valueOf(s))
+                        .average()
+                        .getAsDouble();
+                System.out.println("average of the numbers: " + average);
+                break;
+            }
+            
+            numbers.add(input);
+        }
     }
 }
