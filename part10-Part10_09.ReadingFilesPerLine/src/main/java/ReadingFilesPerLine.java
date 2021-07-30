7514524863e262260c3a2f51b7199fbd7bf3a686
Danila <danpry@st.amu.edu.pl>
2021-07-30 17:16:56 +0200

    10_09 Reading Files Per Line

--- part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -10,8 +10,20 @@ public class ReadingFilesPerLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test the method here
+        
 
+    }
+    
+    public static List<String> read(String file) {
+        List<String> rows = new ArrayList<>();
+        
+        try {
+            Files.lines(Paths.get(file)).forEach(row -> rows.add(row));
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        
+        return rows;
     }
 
 }
