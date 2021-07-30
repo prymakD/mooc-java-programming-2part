d140e422c2569335015d30ae940c4ce6c6b4381c
Danila <danpry@st.amu.edu.pl>
2021-07-30 17:16:32 +0200

    10_10 Reading Files Per Line

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -11,7 +11,21 @@ public class BooksFromFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        
+    }
+    
+    public static List<Book> readBooks(String file) {
+        List<Book> books = new ArrayList<>();
+        
+        try {
+            Files.lines(Paths.get(file))
+                    .map(row -> row.split(","))
+                    .map(parts -> new Book(parts[0], Integer.valueOf(parts[1]), Integer.valueOf(parts[2]), parts[3]))
+                    .forEach(book -> books.add(book));
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return books;
     }
 
 }
