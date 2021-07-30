c7f825f7da6e00ddae65aad05577f66c33de0fc9
Danila <danpry@st.amu.edu.pl>
2021-07-30 17:10:28 +0200

    10_05 PrintingUserInput

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,16 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> strings = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            strings.add(input);
+        }
+        String combined = strings.stream()
+                .reduce("", (s, word) -> s + word + "\n");
+        System.out.println(combined);
     }
 }
