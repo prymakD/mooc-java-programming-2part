c0254dd82dd1a2f8f0bf59fac5792da0f0c6878f
Danila <danpry@st.amu.edu.pl>
2021-08-01 17:33:04 +0200

    10_13 Literacy Comparison
    This exercise uses open data about literacy levels, provided by UNESCO. The data includes statistics of estimated or reported levels of literacy for various countries for the past two years. File literacy.csv, included with the exercise template, includes the literacy estimates for women and men over 15 years of age. Each line in the file literacy.csv is as follows: "theme, age group, gender, country, year, literacy percent. Below are the first five lines as an example.
    
    Create a program that first reads the file literacy.csv and then prints the contents from the lowest to the highest ranked on the literacy rate. The output must be exactly as in the following example. The example shows the first five of the expected rows.

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LiteracyComparison {
     
     public static void main(String[] args) {
-
+        readBooks("literacy.csv").stream().forEach(s -> System.out.println(s));
+    }
+    
+    public static List<String> readBooks(String file) {
+        List<String> literacy = new ArrayList<>();
+        try {
+            Files.lines(Paths.get(file))
+                    .map(row -> row.split(","))
+                    .sorted((x, y) -> { return Double.valueOf(x[5]) < Double.valueOf(y[5]) ? -1 : 1;})
+                    .forEach(row -> literacy.add(row[3] + " (" + row[4] + "), " + row[2].replaceAll("[^a-zA-Z]", "") + ", " + row[5]));
+            return literacy;
+                    
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+        return null;
     }
 }
