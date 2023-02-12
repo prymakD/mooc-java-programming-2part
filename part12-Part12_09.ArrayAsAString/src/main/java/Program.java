59e71ac1e78f2ea99843c58c97ba6616041589fe
Danila <danpry@st.amu.edu.pl>
2023-02-12 16:19:18 +0100

    Part12_09.ArrayAsAString
    Create in the exercise base a method called public static String arrayAsString(int[][] array). It should create a string representation of the array it receives as the parameter and return it.

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -2,7 +2,23 @@
 public class Program {
 
     public static void main(String[] args) {
-        // Test your method here
+        int[][] matrix = {
+            {3, 2, 7, 6},
+            {2, 4, 1, 0},
+            {3, 2, 1, 0}
+        };
+
+        System.out.println(arrayAsString(matrix));
     }
 
+    public static String arrayAsString(int[][] array) {
+        StringBuilder arrayS = new StringBuilder();
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                arrayS.append(array[row][column]);
+            }
+            arrayS.append("\n");
+        }
+        return arrayS.toString();
+    }
 }
